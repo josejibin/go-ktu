a4afafcf3e458f6816820b688ee872b03bf8d2b3
jibin jose <jibin.jacob13@gmail.com>
2016-08-19 10:46:03 +0530

    create a model institute

--- ktu/institutes/models.py
+++ ktu/institutes/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+from django.db import models
+from ktu.base.models import TimeAuditModel
+
+
+@python_2_unicode_compatible
+class Institute(TimeAuditModel):
+
+    name = models.CharField(max_length=100)
+    aicte_id = models.IntegerField(null=True, blank=True)
+    institute_id = models.CharField(max_length=100)
+    address = models.TextField(
+        verbose_name="Address", null=True, blank=True)
+    phone = models.CharField(
+        verbose_name="Office Mobile No", max_length=20, null=True, blank=True)
+    fax = models.CharField(
+        verbose_name="Office Mobile No", max_length=20, null=True, blank=True)
+    email = models.EmailField(
+        verbose_name="Office E-mail", null=True, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
